cd1dbae0338467e7b42e10bf6e7f171ade7654f1
Jim van Zuidam <jimvanzuidam@hotmail.nl>
2014-11-24 10:31:57 +0100

    Roy and Jim: Creating the style for the adventure sheet.

---
+++ adventure_sheet.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Adventure sheet</title>
+		<link href='css/style.css' rel='stylesheet'>
+	</head>
+	<body>
+		<div class="wrapper">
+			<div class="leftWrapper">
+				<div class="itemWrapper">
+					<div class="itemS">
+						<h2>Skill</h2>
+					</div>
+					<div class="itemS">
+						<h2>Stamina</h2>
+					</div>
+					<div class="itemS">
+						<h2>Luck</h2>
+					</div>
+				</div>
+				<div class="itemWrapper">
+					<div style="padding: 5px;" class="leftWrapper">
+						<div class="box inventory">
+							<h2>Items of equipment carried</h2>
+						</div>
+					</div>
+					<div style="padding: 5px;" class="rightWrapper">
+						<div class="box gold">
+							<h2>Gold</h2>
+						</div>
+
+						<div class="box jewels">
+							<h2>Jewels</h2>
+						</div>
+
+						<div class="box potions">
+							<h2>Potions</h2>
+						</div>
+
+						<div class="box provisions">
+							<h2>Provisions remaining</h2>
+						</div>
+
+					</div>
+				</div>
+			</div>
+			<div class="rightWrapper">
+			</div>
+			<div style="clear: both;"></div>
+		</div>
+
+	</body>
+</html>
